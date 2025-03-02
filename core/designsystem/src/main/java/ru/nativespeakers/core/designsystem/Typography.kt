2735ef527d20c251467754f2a1b6eae7af729896
IlyaSekunov <sekunovila@gmail.com>
2025-03-02 13:15:51 +0300

    rework Typography.kt

--- core/designsystem/src/main/java/ru/nativespeakers/core/designsystem/Typography.kt
+++ core/designsystem/src/main/java/ru/nativespeakers/core/designsystem/Typography.kt
@@ -2,50 +2,64 @@ package ru.nativespeakers.core.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 internal val Typography = Typography(
-    displayLarge = TextStyle(
+    displayMedium = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Bold,
         fontSize = 32.sp
     ),
-    displayMedium = TextStyle(
-        fontWeight = FontWeight.Bold,
+    displaySmall = TextStyle(
+        fontFamily = FontFamily.Serif,
+        fontWeight = FontWeight.Medium,
         fontSize = 26.sp
     ),
-    displaySmall = TextStyle(
-        fontWeight = FontWeight.Medium,
-        fontSize = 24.sp
-    ),
     headlineLarge = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp
     ),
     headlineMedium = TextStyle(
-        fontWeight = FontWeight.Medium,
+        fontFamily = FontFamily.Serif,
+        fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     headlineSmall = TextStyle(
+        fontFamily = FontFamily.Serif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Serif,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp
+    ),
+    titleMedium = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Normal,
-        fontSize = 18.sp
+        fontSize = 16.sp
     ),
     bodyLarge = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp
     ),
     bodyMedium = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
-    bodySmall = TextStyle(),
     labelLarge = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp
     ),
     labelMedium = TextStyle(
+        fontFamily = FontFamily.Serif,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
-    labelSmall = TextStyle()
 )
