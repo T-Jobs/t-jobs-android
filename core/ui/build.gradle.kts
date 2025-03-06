0ea118817fe1072e8cdbffd574ad9eece213a319
IlyaSekunov <sekunovila@gmail.com>
2025-03-06 20:14:56 +0300

    Interview status card create

--- core/ui/build.gradle.kts
+++ core/ui/build.gradle.kts
@@ -8,5 +8,13 @@ android {
 }
 
 dependencies {
+    api(projects.core.designsystem)
+
+    api(libs.androidx.compose.foundation)
+    api(libs.androidx.compose.material3)
     api(libs.androidx.core.ktx)
+    api(libs.coil.kt.compose)
+    api(libs.kotlinx.datetime)
+    api(libs.shimmer.compose)
+    api(libs.androidx.compose.material.iconsExtended)
 }
