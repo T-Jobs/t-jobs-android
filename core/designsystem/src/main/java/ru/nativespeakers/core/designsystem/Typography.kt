262ba8795058798cd4e83a960fc7d18fab3d078b
IlyaSekunov <sekunovila@gmail.com>
2025-03-06 21:42:54 +0300

    Add Inter font family

--- core/designsystem/src/main/java/ru/nativespeakers/core/designsystem/Typography.kt
+++ core/designsystem/src/main/java/ru/nativespeakers/core/designsystem/Typography.kt
@@ -2,63 +2,72 @@ package ru.nativespeakers.core.designsystem
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
+private val InterFontFamily = FontFamily(
+    Font(R.font.inter_bold, FontWeight.Bold),
+    Font(R.font.inter_regular, FontWeight.Normal),
+    Font(R.font.inter_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.inter_medium, FontWeight.Medium)
+)
+
 internal val Typography = Typography(
     displayMedium = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 32.sp
     ),
     displaySmall = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 26.sp
     ),
     headlineLarge = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 24.sp
     ),
     headlineMedium = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
     headlineSmall = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 20.sp
     ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp
     ),
     titleMedium = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp
     ),
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Medium,
-        fontSize = 16.sp
+        fontSize = 14.sp
     ),
     bodyMedium = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     labelLarge = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp
     ),
     labelMedium = TextStyle(
-        fontFamily = FontFamily.Serif,
+        fontFamily = InterFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 12.sp
     ),
