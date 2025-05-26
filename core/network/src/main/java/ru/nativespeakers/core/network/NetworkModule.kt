6f5e7acc03c759c991960f5f846543975a97d11a
IlyaSekunov <sekunovila@gmail.com>
2025-05-26 21:51:19 +0300

    Fix bug when switching between accounts

--- core/network/src/main/java/ru/nativespeakers/core/network/NetworkModule.kt
+++ core/network/src/main/java/ru/nativespeakers/core/network/NetworkModule.kt
@@ -6,15 +6,14 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.ANDROID
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.client.plugins.logging.Logger
 import io.ktor.client.plugins.logging.Logging
+import io.ktor.client.request.HttpRequestPipeline
+import io.ktor.http.HttpHeaders
 import io.ktor.http.URLProtocol
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.datetime.LocalDateTime
@@ -37,6 +36,17 @@ internal object NetworkModule {
             })
         }
 
+        install("AuthInterceptor") {
+            requestPipeline.intercept(HttpRequestPipeline.Before) {
+                with(context.headers) {
+                    remove(HttpHeaders.Authorization)
+                    tokenLocalDataSource.token.value?.let {
+                        append(HttpHeaders.Authorization, "Bearer $it")
+                    }
+                }
+            }
+        }
+
         install(Logging) {
             level = LogLevel.ALL
             logger = Logger.ANDROID
@@ -46,19 +56,6 @@ internal object NetworkModule {
             host = "10.0.2.2"
             port = 8080
             url { protocol = URLProtocol.HTTP }
-        }
-
-        install(Auth) {
-            bearer {
-                loadTokens {
-                    val token = tokenLocalDataSource.token.value ?: return@loadTokens null
-                    BearerTokens(accessToken = token, refreshToken = null)
-                }
-                refreshTokens {
-                    val token = tokenLocalDataSource.token.value ?: return@refreshTokens null
-                    BearerTokens(accessToken = token, refreshToken = null)
-                }
-            }
         }
     }
 }
