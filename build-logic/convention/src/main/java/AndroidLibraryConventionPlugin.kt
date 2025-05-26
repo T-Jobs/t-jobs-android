58c6464ae3c54821ccbd6b385175af56706e6936
IlyaSekunov <sekunovila@gmail.com>
2025-05-26 20:54:33 +0300

    Delete useless dependencies

--- build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/java/AndroidLibraryConventionPlugin.kt
@@ -3,10 +3,8 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.dependencies
 import ru.nativespeakers.convention.configureFlavors
 import ru.nativespeakers.convention.configureKotlinAndroid
-import ru.nativespeakers.convention.libs
 
 class AndroidLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -23,11 +21,6 @@ class AndroidLibraryConventionPlugin : Plugin<Project> {
                 // The resource prefix is derived from the module name,
                 // so resources inside ":core:module1" must be prefixed with "core_module1_"
                 resourcePrefix = path.split("""\W""".toRegex()).drop(1).distinct().joinToString(separator = "_").lowercase() + "_"
-            }
-
-            dependencies {
-                "androidTestImplementation"(libs.findLibrary("kotlin.test").get())
-                "testImplementation"(libs.findLibrary("kotlin.test").get())
             }
         }
     }
